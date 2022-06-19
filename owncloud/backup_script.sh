0502ae33446478d727db59ad2b681a00cb12e50d
Andreas Georgiou <geo.andreasyeah@gmail.com>
2022-06-19 17:49:13 +0200

    Fixed owncloud backup script

--- owncloud/backup_script.sh
+++ owncloud/backup_script.sh
@@ -4,7 +4,7 @@ mkdir -p /mnt/backup/owncloud
 (
 flock -x 200
 occ maintenance:mode --on
-rsync -Aax config data apps apps-external /mnt/backup/owncloud/
+rsync -Aax --ignore-missing-args /mnt/data/config /mnt/data/files /mnt/data/apps /mnt/data/apps-external /mnt/backup/owncloud/
 mysqldump --single-transaction -h mariadb -u owncloud --password=owncloud owncloud --column-statistics=0 > /mnt/backup/owncloud/owncloud-dbbackup_`date +"%Y%m%d"`.bak
 )200>/mnt/backup/.lockfile
 
