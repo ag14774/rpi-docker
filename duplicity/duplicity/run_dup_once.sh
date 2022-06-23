a92df4ae7d6ec4528dacba7a10716b1149d04ca4
Andreas Georgiou <geo.andreasyeah@gmail.com>
2022-06-23 21:03:52 +0200

    Updated duplicity images to use alpine

--- duplicity/duplicity/run_dup_once.sh
+++ duplicity/duplicity/run_dup_once.sh
@@ -1,22 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 export PASSPHRASE="<duplicity-password>"
 
 OFFSITE_FULL_EVERY="90D"
 OFFSITE_KEEP_FULL=1
 OFFSITE_DEST=<duplicity-offsite-dest>
+# Example OFFSITE_DEST=gdrive://4543653-565gfdgdf.apps.googleusercontent.com?myDriveFolderID=543gfFG4gfdv54vfd44
+# The googleusercontent.com url can be found in console.cloud.google.com under 'Client ID' in the Credentials tab
+# The myDriveFolderID can be found in the url of google drive inside the folder you would like to backup your data
+# e.g https://drive.google.com/drive/u/0/folders/543gfFG4gfdv54vfd44 <--folder id
 VERBOSITY=3
 
 (
 flock -x 200
 
 # Clean up any previously failed runs
-GOOGLE_DRIVE_SETTINGS=/duplicity/credentials duplicity --verbosity=0 cleanup --force ${OFFSITE_DEST}
+GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+  duplicity --verbosity=$VERBOSITY cleanup --force ${OFFSITE_DEST}
 
 if [ $VERBOSITY -gt 0 ]; then
     echo "Performing an offsite backup to ${OFFSITE_DEST}"
 fi
-GOOGLE_DRIVE_SETTINGS=/duplicity/credentials duplicity \
+GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+  duplicity \
     --verbosity=$VERBOSITY \
     --allow-source-mismatch \
     --full-if-older-than=$OFFSITE_FULL_EVERY \
@@ -26,7 +32,8 @@ GOOGLE_DRIVE_SETTINGS=/duplicity/credentials duplicity \
 if [ $VERBOSITY -gt 0 ]; then
     echo "Removing all but ${OFFSITE_KEEP_FULL} full backup from ${OFFSITE_DEST}"
 fi
-GOOGLE_DRIVE_SETTINGS=/duplicity/credentials duplicity --verbosity=$VERBOSITY \
+GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+  duplicity --verbosity=$VERBOSITY \
     remove-all-but-n-full ${OFFSITE_KEEP_FULL} \
     --force ${OFFSITE_DEST}
 
