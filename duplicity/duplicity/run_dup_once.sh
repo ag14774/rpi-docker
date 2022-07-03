9ec4ccd13b644244e36b586cb3c1ff4089a49a1e
Andreas Georgiou <ag14774@users.noreply.github.com>
2022-07-03 12:56:15 +0200

    Switched to using service accounts for gdrive (#1)

--- duplicity/duplicity/run_dup_once.sh
+++ duplicity/duplicity/run_dup_once.sh
@@ -9,19 +9,21 @@ OFFSITE_DEST=<duplicity-offsite-dest>
 # The googleusercontent.com url can be found in console.cloud.google.com under 'Client ID' in the Credentials tab
 # The myDriveFolderID can be found in the url of google drive inside the folder you would like to backup your data
 # e.g https://drive.google.com/drive/u/0/folders/543gfFG4gfdv54vfd44 <--folder id
+# For service accounts the offise destination would look like: 
+# gdrive://gdfgdf@gfdgdfs.iam.gserviceaccount.com/?myDriveFolderId=gfgrgeg43
 VERBOSITY=3
 
 (
 flock -x 200
 
 # Clean up any previously failed runs
-GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+GOOGLE_SERVICE_JSON_FILE=/duplicity/credentials.json \
   duplicity --verbosity=$VERBOSITY cleanup --force ${OFFSITE_DEST}
 
 if [ $VERBOSITY -gt 0 ]; then
     echo "Performing an offsite backup to ${OFFSITE_DEST}"
 fi
-GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+GOOGLE_SERVICE_JSON_FILE=/duplicity/credentials.json \
   duplicity \
     --verbosity=$VERBOSITY \
     --allow-source-mismatch \
@@ -32,7 +34,7 @@ GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FI
 if [ $VERBOSITY -gt 0 ]; then
     echo "Removing all but ${OFFSITE_KEEP_FULL} full backup from ${OFFSITE_DEST}"
 fi
-GOOGLE_CLIENT_SECRET_JSON_FILE=/duplicity/credentials.json GOOGLE_CREDENTIALS_FILE=/mnt/backup/gdrive.cache \
+GOOGLE_SERVICE_JSON_FILE=/duplicity/credentials.json \
   duplicity --verbosity=$VERBOSITY \
     remove-all-but-n-full ${OFFSITE_KEEP_FULL} \
     --force ${OFFSITE_DEST}
